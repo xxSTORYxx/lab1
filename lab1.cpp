76794bb79c400d5ceccbed3be19d35d99517bb65
xxSTORYxx <villie.liang@gmail.com>
2017-03-17 19:41:45 +0800

    lab1

---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ( void ){
+	int n;
+	cin >> n;
+	cout << n << " "; 
+	while ( n != 1 ){
+		if ( n % 2 == 1 ){
+			n = 3*n + 1;
+		}
+		else{
+			n = n/2;
+		}
+
+		cout << n << " "; 
+	};
+	return 0;
+}
